b290039b2c962154c5c607eb6482f245b3098b62
Crystal Harper <crystalhaylen@gmail.com>
2021-06-15 20:11:16 -0500

    Dog website, added gitignore

--- dogsite/settings.py
+++ dogsite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from dogsite.secrets import SECRET_KEY
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pd!o302oeg1&awqx11qeo=^vqzc!ue2clysf=)r8%^$0u$4ff0'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
